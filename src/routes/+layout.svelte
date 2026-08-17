0b20e5e310c1ecff06976276bb80473b218f0c5d
Enis Budancamanak <Enis.Budancamanak@hotmail.com>
2026-08-18 01:25:53 +0200

    fix: put the icon links in the html shell

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,11 +5,4 @@
 	let { children } = $props();
 </script>
 
-<svelte:head>
-	<link rel="icon" href="/icon.svg" type="image/svg+xml" />
-	<link rel="apple-touch-icon" href="/icon.svg" />
-	<link rel="manifest" href="/manifest.webmanifest" />
-	<meta name="theme-color" content="#000000" />
-</svelte:head>
-
 {@render children()}
